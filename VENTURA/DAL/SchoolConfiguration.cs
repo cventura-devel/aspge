f72839c7f39945c71cf0c0a3572d414d7d0a5c5c
Carlos Ventura <cventura@outlook.com>
2016-01-19 22:57:23 -0700

    first upload

---
+++ VENTURA/DAL/SchoolConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace VENTURA.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
