7f260e7b30271f6b8183440885e5e6263cca6d1b
Carlos Ventura <cventura@outlook.com>
2016-02-02 21:42:13 -0700

    last

--- VENTURA/DAL/SchoolContext.cs
+++ VENTURA/DAL/SchoolContext.cs
@@ -27,6 +27,8 @@ namespace VENTURA.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
+
         }
     }
 }
